10ff69d34d973d8f91c6ba622bd2e9465d2ad365
Maurizio Pilia <mpilia@Mac.local>
2016-09-22 18:59:31 +0200

    mail correcto en google script

--- static/js/main.js
+++ static/js/main.js
@@ -3,20 +3,20 @@ $( document ).ready( function () {
         e.preventDefault();
         e.stopPropagation();
 
+        var url = 'https://script.google.com/macros/s/AKfycby9-PNIjZmZ5PwdrthBRNofNyeCEPG3EAZ2aZa4IQLyjPyPA0vF/exec';
         var form = new FormData();
         form.append('nom', $('[name="nom"]').val());
         form.append('telefon', $('[name="telefon"]').val());
         form.append('descricion', $('[name="descricion"]').val());
         form.append('email', $('[name="email"]').val());
-        console.log(form);
+
         $.ajax({
-            url: "https://script.google.com/macros/s/AKfycby9-PNIjZmZ5PwdrthBRNofNyeCEPG3EAZ2aZa4IQLyjPyPA0vF/exec",
+            url: url,
             type: 'POST',
             data: form,
             processData: false,
             contentType: false
         }).done(function( data ) {
-            console.log(data);
             window.location.href = 'index.html';
         });
     } );
